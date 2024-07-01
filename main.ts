e127fc8867669d4714045a4832b495207eb1f394
jcollingj <jcollingj@gmail.com>
2024-07-01 11:43:11 -0600

    update version

--- main.ts
+++ main.ts
@@ -46,7 +46,7 @@ import { CaretCanvas } from "./caret_canvas";
 const parseString = require("xml2js").parseString;
 
 export const DEFAULT_SETTINGS: CaretPluginSettings = {
-    caret_version: "0.2.32",
+    caret_version: "0.2.33",
     chat_logs_folder: "caret/chats",
     chat_logs_date_format_bool: false,
     chat_logs_rename_bool: true,
