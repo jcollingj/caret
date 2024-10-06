e561198c3e6d0702458c5aeef3a2ece4615861b2
jcollingj <jcollingj@gmail.com>
2024-10-06 11:00:23 -0600

    Adding split and connect to parent

--- main.ts
+++ main.ts
@@ -35,7 +35,7 @@ import { CaretCanvas } from "./caret_canvas";
 const parseString = require("xml2js").parseString;
 
 export const DEFAULT_SETTINGS: CaretPluginSettings = {
-    caret_version: "0.2.60",
+    caret_version: "0.2.61",
     chat_logs_folder: "caret/chats",
     chat_logs_date_format_bool: false,
     chat_logs_rename_bool: true,
@@ -1800,7 +1800,7 @@ version: 1
                                     const newNode = canvas.nodes?.get(newNodeTemp?.id!);
                                     if (newNode) {
                                         // Add a link from the original node to the new node
-                                        await this.createEdge(node,newNode, canvas,  "right");
+                                        await this.createEdge(node, newNode, canvas, "right");
                                     }
                                     canvas.requestFrame();
                                 } catch (error) {
